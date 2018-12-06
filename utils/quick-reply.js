e98f132b981f715c4dd36e7a2f17909ae66a14aa
Adrian Najera <adrian.quezada@azka-it.com>
2018-12-06 16:41:45 -0600

    More changes

--- utils/quick-reply.js
+++ utils/quick-reply.js
@@ -20,7 +20,7 @@ function sendQuickReply(psid, text, title, postback_payload) {
   payload.recipient = {
     id: psid
   }
-console.log(payload.recipient.id);
+ console.log("Here is the RecipientID: "+payload.recipient.id);
   payload.message = {
     text: text,
     quick_replies: [{
