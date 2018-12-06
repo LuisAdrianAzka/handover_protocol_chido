f71a33ac5d57d38dd0e02fe41729df3c01284b35
Adrian Najera <adrian.quezada@azka-it.com>
2018-12-06 16:17:44 -0600

    Is echo?

--- utils/quick-reply.js
+++ utils/quick-reply.js
@@ -12,22 +12,23 @@ const api = require('./api');
 
 // Send a quick reply message
 function sendQuickReply(psid, text, title, postback_payload) {
-  
+
   console.log('SENDING QUICK REPLY');
-  
+
   let payload = {};
-  
+
   payload.recipient = {
     id: psid
   }
 
   payload.message = {
+    is_echo:true,
     text: text,
     quick_replies: [{
         content_type: 'text',
         title: title,
         payload: postback_payload
-    }]    
+    }]
   }
 
   api.call('/messages', payload, () => {});
